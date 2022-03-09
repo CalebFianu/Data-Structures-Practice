2822b2db636b590df5ff507f576d3c8ceae1833c
CalebFianu <caleb.seyram.fianu@gmail.com>
2022-03-09 21:11:17 +0000

    Added selection sort!

---
+++ selectsort.py
@@ -0,0 +1,14 @@
+def selectsort(A):
+    for i in range(len(A)-1, 0, -1):
+        max_position = 0
+        for j in range(1, i+1):
+            if A[j] > A[max_position]:
+                max_position = j
+        temp = A[i]
+        A[i] = A[max_position]
+        A[max_position] = temp
+
+A = [1,4,2,5,8]
+print('Your original array is ', A)
+selectsort(A)
+print('Your sorted array is ', A)
