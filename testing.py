fb362c020043d21ff8732e4d2bedf9190cec5c9a
CalebFianu <caleb.seyram.fianu@gmail.com>
2022-06-06 12:20:43 +0000

    testing some questions

--- testing.py
+++ testing.py
@@ -3,13 +3,29 @@ firstline = input().split(" ")
 #K = firstline[1]
 
 arrLen = int(firstline[0])
+k = int(firstline[1])
 arr = []
 while len(arr) < arrLen:
     arr = input().split(" ")
 
 
+
+
+# we'll use a hashmap/array of some sort to store the array. then we will add the indexes of k to that array
+# sort it, and return the middle index. Done!
+# new = {}
+# for i in range(len(arr)):
+#     new[i] = int(arr[i])
+
+# new_arr = []
+# if new.get(i) == k:
+#     new_arr.append(i)
+# print(new_arr)
+
+# for i in range(arr) if k in
 # print(firstline[::-1])
-if firstline[1] in arr:
-    print(arr.index(firstline[1]))
-else:
-    print('K is not present')
+
+# if firstline[1] in arr:
+#     print(arr.index(firstline[1]))
+# else:
+#     print('K is not present')
